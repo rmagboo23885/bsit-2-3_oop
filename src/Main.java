4735657b33775bdfeadc410d9d8dbe44ad965535
rmagboo23885 <rmagboo23885@liceo.edu.ph>
2025-08-04 15:12:50 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+
+        System.out.println("=== SUM OF 5 NUMBERS ===");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.print("Invalid input. Please enter a valid number: ");
+                scanner.next();
+            }
+
+            int number = scanner.nextInt();
+            total += number;
+        }
+
+        System.out.println("\nTotal Sum: " + total);
+        scanner.close();
     }
 }
