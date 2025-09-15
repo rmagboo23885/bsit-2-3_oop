3908ae934f21a3fcaf5a3c244bfc759fdd54d4cf
rmagboo23885 <rmagboo23885@liceo.edu.ph>
2025-09-15 15:46:10 +0800

    Update Developer.java

--- src/Developer.java
+++ src/Developer.java
@@ -1,1 +1,28 @@
+public class Developer extends Employee {
+    private String programmingLanguage;
+    private int projectsCompleted;
 
+    public Developer(String name, int employeeId, double baseSalary, String department, String programmingLanguage, int projectsCompleted) {
+        super(name, employeeId, baseSalary, department); // Call parent constructor
+        this.programmingLanguage = programmingLanguage;
+        this.projectsCompleted = projectsCompleted;
+        System.out.println(name + " joined as a " + programmingLanguage + " Developer");
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary + (projectsCompleted * 1000); // Base + $1000 per project
+    }
+
+    @Override
+    public void work() {
+        System.out.println(name + " is coding in " + programmingLanguage);
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo(); // Display parent info
+        System.out.println("Programming Language: " + programmingLanguage);
+        System.out.println("Projects Completed: " + projectsCompleted);
+    }
+}
