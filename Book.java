7a8b646aa7997af85e2aa2d2383931a34e05d99c
rmagboo23885 <rmagboo23885@liceo.edu.ph>
2025-09-26 22:00:27 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,18 @@
+public class Book extends Media {
+    private String author;
 
+    public Book(String title, String itemId, String author) {
+        super(title, itemId);
+        this.author = author;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Book";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: \"" + title + "\" by " + author + " (ID: " + itemId + ")");
+    }
+}
