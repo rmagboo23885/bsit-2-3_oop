199b60c7d59d52734f5821db46e5d4dafbe0589b
rmagboo23885 <rmagboo23885@liceo.edu.ph>
2025-09-26 22:01:29 +0800

    Update DVD.java

--- DVD.java
+++ DVD.java
@@ -1,1 +1,18 @@
+public class DVD extends Media {
+    private String director;
 
+    public DVD(String title, String itemId, String director) {
+        super(title, itemId);
+        this.director = director;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "DVD";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("DVD: \"" + title + "\" directed by " + director + " (ID: " + itemId + ")");
+    }
+}
