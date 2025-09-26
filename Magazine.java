cff1caf1d0b9cc73b334e0517c308d0ef0ec62ad
rmagboo23885 <rmagboo23885@liceo.edu.ph>
2025-09-26 22:00:52 +0800

    Update Magazine.java

--- Magazine.java
+++ Magazine.java
@@ -1,1 +1,18 @@
+public class Magazine extends Media {
+    private int issueNumber;
 
+    public Magazine(String title, String itemId, int issueNumber) {
+        super(title, itemId);
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Magazine";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Magazine: \"" + title + "\" Issue #" + issueNumber + " (ID: " + itemId + ")");
+    }
+}
